586130f9b861805feff5e0aeb29506d00e8f8f8c
VisViva <a.vis.viva@gmail.com>
2016-01-11 22:17:15 +0400

    gulp works fine now

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp'),
-    gp_concat = require('gulp-concat');
+    del = require('del'),
+    gp_concat = require('gulp-concat'),
+    gp_replace = require('gulp-replace');
+
 
 var scripts = [
     // Jquery
@@ -26,33 +29,98 @@ var scripts = [
     'public/js/app.js',
 ]
 
-var stylesheets = [
-    // Bootstrap
-    'public/libs/bootstrap/dist/css/bootstrap.css',
-    // Font awesome
-    'public/libs/font-awesome/css/font-awesome.css',
-    // Lato fonts
-    'public/libs/lato/css/lato.css',
-    // Open sans fonts
-    'public/libs/open-sans-fontface/open-sans.css',
-    // Malihu custom scrollbar
-    'public/libs/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css',
+var stylesheets = {
+    // Styles
+    styles:[
+        // Malihu custom scrollbar
+        'public/libs/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css',
 
-    // Custom styles
-    'public/css/style.css',
-    'public/css/responsive.css'
+        // Custom styles
+        'public/css/style.css',
+        'public/css/responsive.css'
+    ],
+
+    // Bootstrap
+    bootstrap:'public/libs/bootstrap/dist/css/bootstrap.css',
+    // Font awesome
+    fontawesome:'public/libs/font-awesome/css/font-awesome.css',
+    // Lato fonts
+    lato:'public/libs/lato/css/lato.css',
+    // Open sans fonts
+    opensans:'public/libs/open-sans-fontface/open-sans.css'
+}
+
+var fonts = [
+    // Bootstrap
+    'public/libs/bootstrap/dist/fonts/**/*',
+    // Font awesome
+    'public/libs/font-awesome/fonts/**/*',
+    // Lato
+    'public/libs/lato/font/**/*',
+    // Open sans
+    'public/libs/open-sans-fontface/fonts/**/*'
 ]
 
-gulp.task('scripts', function(){
+// Low level tasks
+
+gulp.task('styles', ['cleanup'], function(){
+    return gulp.src(stylesheets.styles)
+        .pipe(gp_replace("url(../img", "url(../../img"))
+        .pipe(gp_concat('styles.css'))
+        .pipe(gulp.dest('public/dist/css/temp'));
+});
+
+gulp.task('bootstrap', ['cleanup'], function(){
+    return gulp.src(stylesheets.bootstrap)
+        .pipe(gulp.dest('public/dist/css/temp'));
+});
+
+gulp.task('fontawesome', ['cleanup'], function(){
+    return gulp.src(stylesheets.fontawesome)
+        .pipe(gulp.dest('public/dist/css/temp'));
+});
+
+gulp.task('lato', ['cleanup'], function(){
+    return gulp.src(stylesheets.lato)
+        .pipe(gp_replace("../font", "../fonts"))
+        .pipe(gulp.dest('public/dist/css/temp'));
+});
+
+gulp.task('opensans', ['cleanup'], function(){
+    return gulp.src(stylesheets.opensans)
+        .pipe(gp_replace("./fonts", "../fonts"))
+        .pipe(gulp.dest('public/dist/css/temp'));
+});
+
+gulp.task('combine-stylesheets', ['styles', 'bootstrap', 'fontawesome', 'lato', 'opensans'], function(){
+    return gulp.src('public/dist/css/temp/*')
+        .pipe(gp_concat('stylesheets.css'))
+        .pipe(gulp.dest('public/dist/css'));
+});
+
+gulp.task('cleanup-temp', ['combine-stylesheets'], function(){
+    return del('public/dist/css/temp');
+});
+
+// High level tasks
+
+gulp.task('scripts', ['cleanup'], function(){
     return gulp.src(scripts)
         .pipe(gp_concat('scripts.js'))
-        .pipe(gulp.dest('public/deploy'));
+        .pipe(gulp.dest('public/dist/js'));
 });
 
-gulp.task('stylesheets', function(){
-    return gulp.src(stylesheets)
-        .pipe(gp_concat('stylesheets.css'))
-        .pipe(gulp.dest('public/deploy'));
+gulp.task('stylesheets', ['styles', 'bootstrap', 'fontawesome', 'lato', 'opensans', 'combine-stylesheets', 'cleanup-temp']);
+
+gulp.task('fonts', ['cleanup'], function(){
+    return gulp.src(fonts)
+        .pipe(gulp.dest('public/dist/fonts'));
 });
 
-gulp.task('default', ['scripts', 'stylesheets']);
+gulp.task('cleanup', function(){
+    return del('public/dist/*');
+});
+
+// Default task
+
+gulp.task('default', ['cleanup', 'scripts', 'stylesheets', 'fonts']);
