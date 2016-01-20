071ae5ce393d9a6fc90bc7641791dd30baeaa422
VisViva <a.vis.viva@gmail.com>
2016-01-21 00:03:20 +0400

    Fixed mngolab connection

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port : process.env.PORT || 8080,
-  url : 'mongodb://heroku_4v694zhj:af22re9k0epsbdqm4fod3s5adn@ds047325.mongolab.com:47325/heroku_4v694zhj',
+  url : 'mongodb://visviva:gh7df90mbyy@ds047325.mongolab.com:47325/heroku_4v694zhj',
   web : '/public'
 }
