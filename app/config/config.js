8ba0b775ccd0070bf3a2688b89dcb44cee18077c
VisViva <a.vis.viva@gmail.com>
2016-01-26 22:09:27 +0400

    Design changes mostly

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   port : process.env.PORT || 8080,
-  url : 'mongodb://visviva:gh7df90mbyy@ds047325.mongolab.com:47325/heroku_4v694zhj',
+  url : 'mongodb://localhost/test',
+  //url : 'mongodb://visviva:gh7df90mbyy@ds047325.mongolab.com:47325/heroku_4v694zhj',
   web : '/public'
 }
