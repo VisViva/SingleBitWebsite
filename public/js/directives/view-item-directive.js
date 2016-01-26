cef86ad7352c3b2de80df3fd4a7eb8d8071a1027
VisViva <a.vis.viva@gmail.com>
2016-01-26 22:37:14 +0400

    Added placeholders

--- public/js/directives/view-item-directive.js
+++ public/js/directives/view-item-directive.js
@@ -2,13 +2,16 @@ angular.module('ViewItemDirective', []).directive('viewItem', function ($compile
   return {
     scope: {
       model: '=ngModel',
+      placeholder: '@placeholder'
     },
     restrict: 'E',
     link: function(scope, element){
       scope.showIf = function(type){
         return (scope.model.resourceType == type);
       }
-
+      scope.showThumbnailIfExists = function(){
+        return !((angular.isUndefined(scope.model.thumbnail)) || (scope.model.thumbnail.length == 0));
+      }
       var template = '<div class="view-item">\
                         <div class="hover-bg">\
                           <a href="#">\
@@ -18,7 +21,8 @@ angular.module('ViewItemDirective', []).directive('viewItem', function ($compile
                               <div class="clearfix"></div>\
                               <i class="fa fa-arrow-right"></i>\
                             </div>\
-                            <img ng-src="{{model.thumbnail}}" class="img-responsive full-width" alt="...">\
+                            <img ng-show="showThumbnailIfExists();" ng-src="{{model.thumbnail}}" class="img-responsive full-width">\
+                            <img ng-show="!showThumbnailIfExists();" ng-src="{{placeholder}}" class="img-responsive full-width">\
                             <div style="top: 100%; transform: translateY(-100%); position: absolute; width: 100px;">\
                               <i ng-show="showIf(' + "'Music'" + ')"class="fa fa-music" style="color: #FFF;z-index:0;margin-left: 15px;margin-bottom: 3px;height: 60px;width: 60px;font-size: 50px;padding:0px;border:none;text-shadow: 1px 1px 1px #000;"></i>\
                               <i ng-show="showIf(' + "'Diary'" + ')"class="fa fa-youtube" style="color: #FFF;z-index:0;margin-left: 10px;margin-bottom: 10px;height: 60px;width: 60px;font-size: 60px;padding:0px;border:none;text-shadow: 1px 1px 1px #000;"></i>\
