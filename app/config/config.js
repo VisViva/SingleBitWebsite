5d730c82121d2b07d944974550ef2828a8304a4c
visviva <vis.viva@live.com>
2016-02-16 10:19:42 +0400

    AWS

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port : process.env.PORT || 80,
+  port : 80,
   //url : 'mongodb://localhost/test',
   url : 'mongodb://visvivamongo:gh7df90mbyy@ec2-52-36-176-9.us-west-2.compute.amazonaws.com:27017/singlebit',
   //url : 'mongodb://visviva:gh7df90mbyy@ds047325.mongolab.com:47325/heroku_4v694zhj',
