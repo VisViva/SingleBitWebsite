bcd7f46ecb0ca1945723a1ea88baf7a9aa52e3c6
Vis Viva <a.vis.viva@gmail.com>
2016-06-21 14:52:13 +0400

    Added software projects to the project types view

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port : process.env.PORT || 8080,
-  //url : 'mongodb://localhost/singlebit',
-  url : 'mongodb://visvivamongo:gh7df90mbyy@ec2-52-36-176-9.us-west-2.compute.amazonaws.com:27017/singlebit',
+  url : 'mongodb://localhost/singlebit',
+  //url : 'mongodb://visvivamongo:gh7df90mbyy@ec2-52-36-176-9.us-west-2.compute.amazonaws.com:27017/singlebit',
   web : '/public'
 }
