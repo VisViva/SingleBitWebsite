d4a3eb176d46e5202b0967cc69d67b0acdc9494c
Vis Viva <a.vis.viva@gmail.com>
2016-06-23 16:39:59 +0400

    Mergin upload branch

--- app/config/config.js
+++ app/config/config.js
@@ -2,6 +2,6 @@ module.exports = {
   port : process.env.PORT || 8080,
   web : '/public',
   images_relative_url: '/files/images/',
-  url : 'mongodb://localhost/singlebit'
-  //url : 'mongodb://visvivamongo:gh7df90mbyy@ec2-52-36-176-9.us-west-2.compute.amazonaws.com:27017/singlebit'
+  //url : 'mongodb://localhost/singlebit'
+  url : 'mongodb://visvivamongo:gh7df90mbyy@ec2-52-36-176-9.us-west-2.compute.amazonaws.com:27017/singlebit'
 }
