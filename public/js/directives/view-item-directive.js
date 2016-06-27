bc22032f6397be35e0940a5947b39be75ee66aaa
Vis Viva <a.vis.viva@gmail.com>
2016-06-27 13:47:05 +0400

    Design changes

--- public/js/directives/view-item-directive.js
+++ public/js/directives/view-item-directive.js
@@ -19,7 +19,17 @@ angular.module('ViewItemDirective', []).directive('viewItem', ['$compile', funct
                               <h4 ng-bind="model.title"></h4>\
                               <small ng-bind="model.date | Date"></small>\
                               <div class="clearfix"></div>\
-                              <i class="fa fa-arrow-right"></i>\
+                              <div class="view-item-icon-container">\
+                                <span ng-show="showIf(' + "'Software'" + ')"class="fa fa-code"></span>\
+                                <span ng-show="showIf(' + "'Music'" + ')"class="fa fa-music"></span>\
+                                <span ng-show="showIf(' + "'Tutorial'" + ')"class="fa fa-youtube"></span>\
+                                <span ng-show="showIf(' + "'Blog'" + ')"class="fa fa-rss"></span>\
+                                <span ng-show="showIf(' + "'Podcast'" + ')"class="fa fa-microphone"></span>\
+                                <span ng-show="showIf(' + "'Article'" + ')"class="fa fa-newspaper-o"></span>\
+                                <span ng-show="showIf(' + "'Game'" + ')"class="fa fa-gamepad"></span>\
+                                <span ng-show="showIf(' + "'2D art'" + ')"class="fa fa-paint-brush"></span>\
+                                <span ng-show="showIf(' + "'3D art'" + ')"class="fa fa-cube"></span>\
+                              </div>\
                             </div>\
                             <div class="view-item-image-container">\
                               <div ng-show="showThumbnailIfExists();">\
@@ -30,17 +40,6 @@ angular.module('ViewItemDirective', []).directive('viewItem', ['$compile', funct
                                 <div class="view-item-background"></div>\
                                 <img ng-src="{{placeholder}}" class="img-responsive full-width view-item-thumbnail">\
                               </div>\
-                            </div>\
-                            <div class="view-item-icon-container">\
-                              <span ng-show="showIf(' + "'Software'" + ')"class="fa fa-code software-fix"></span>\
-                              <span ng-show="showIf(' + "'Music'" + ')"class="fa fa-music music-fix"></span>\
-                              <span ng-show="showIf(' + "'Tutorial'" + ')"class="fa fa-youtube tutorial-fix"></span>\
-                              <span ng-show="showIf(' + "'Blog'" + ')"class="fa fa-rss blog-fix"></span>\
-                              <span ng-show="showIf(' + "'Podcast'" + ')"class="fa fa-microphone podcast-fix"></span>\
-                              <span ng-show="showIf(' + "'Article'" + ')"class="fa fa-newspaper-o article-fix"></span>\
-                              <span ng-show="showIf(' + "'Game'" + ')"class="fa fa-gamepad game-fix"></span>\
-                              <span ng-show="showIf(' + "'2D art'" + ')"class="fa fa-paint-brush brush-fix"></span>\
-                              <span ng-show="showIf(' + "'3D art'" + ')"class="fa fa-cube cube-fix"></span>\
                             </div>\
                             <div ng-bind="' + "'#'" + ' + model.number" class="view-item-number">\
                             </div>\
